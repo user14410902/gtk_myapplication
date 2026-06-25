f60d14acc3e3780cc4e0ba99a70567f2e8e14a61
your name <you@example.com>
2026-06-25 13:55:56 -0600

    Adding menu item to show a dialog. Work in progress.

--- my-window.c
+++ my-window.c
@@ -2,11 +2,17 @@
 
 static void my_window_finalize(GObject *object);
 
+static void my_window_show_dialog(GSimpleAction *action, GVariant *value,
+                                  gpointer data);
+
 struct _MyWindow {
   GtkApplicationWindow __parent__;
 
   GtkWidget *menubar;
 };
+
+static const GActionEntry action_entries[] = {
+    {"file.show-dialog", my_window_show_dialog, NULL, NULL, NULL}};
 
 G_DEFINE_TYPE(MyWindow, my_window, GTK_TYPE_APPLICATION_WINDOW)
 
@@ -16,13 +22,13 @@ GtkWidget *my_window_new(MyApplication *application) {
 }
 
 static void my_window_class_init(MyWindowClass *klass) {
-  GObjectClass * gobject_class;
-//  GtkWidgetClass *gtkwidget_class;
-//
+  GObjectClass *gobject_class;
+  //  GtkWidgetClass *gtkwidget_class;
+  //
   gobject_class = G_OBJECT_CLASS(klass);
-  gobject_class->finalize=my_window_finalize;
-//
-//  gtkwidget_class = GTK_WIDGET_CLASS(klass);
+  gobject_class->finalize = my_window_finalize;
+  //
+  //  gtkwidget_class = GTK_WIDGET_CLASS(klass);
 }
 
 static void my_window_finalize(GObject *object) {
@@ -30,5 +36,11 @@ static void my_window_finalize(GObject *object) {
 }
 
 static void my_window_init(MyWindow *window) {
+  g_action_map_add_action_entries(G_ACTION_MAP(window), action_entries,
+                                  G_N_ELEMENTS(action_entries), window);
+}
 
+static void my_window_show_dialog(GSimpleAction *action, GVariant *value,
+                                  gpointer data) {
+  g_print("my_window_show_dialog\n");
 }
